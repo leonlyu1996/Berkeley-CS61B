db9a780144d3ff075e7a1340f9ef1507258a3593
zmkoo000 <zmkoo000@gmail.com>
2018-01-06 21:27:32 +0800

    fixed a minor bug

--- hw1/GuitarHero.java
+++ hw1/GuitarHero.java
@@ -5,17 +5,14 @@ public class GuitarHero {
     private static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
 
     public static void main(String[] args) {
-        /* create two guitar strings, for concert A and C */
 
         GuitarString[] string = new GuitarString[keyboard.length()];
 
         for (int i = 0; i < keyboard.length(); i++) {
-            string[i] = new GuitarString(440.0 * Math.pow(2, (i - 24) / 12));
+            string[i] = new GuitarString(440.0 * Math.pow(2.0, (i - 24.0) / 12.0));
         }
 
-
         while (true) {
-
 
             /* check if the user has typed a key; if so, process it */
             if (StdDraw.hasNextKeyTyped()) {
@@ -25,7 +22,6 @@ public class GuitarHero {
                     string[index].pluck();
                 }
             }
-
 
             /* compute the superposition of samples */
             double sample = 0.0;
